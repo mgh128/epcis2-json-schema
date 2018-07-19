cd8d58fc13be243f24149d50896ea3840281a3a3
Danny Haak <github@dannyhaak.nl>
2018-07-19 14:23:17 +0200

    Making sure either epcList or quantityList is included; working; jay!

---
+++ DotNet/EpcisJsonSchemaValidation/EpcisJsonSchemaValidationTests/AnyOfEpcQuantity/AnyOfEpcQuantity.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Newtonsoft.Json.Schema;
+
+using Xunit;
+
+namespace EpcisJsonSchemaValidationTests.AnyOfEpcQuantity
+{
+    public class AnyOfEpcQuantity : HelperMethods
+    {
+        [Fact]
+        public void ObjectEventPassEpcList()
+        {
+            var json = GetJson(GetType().Name, MethodBase.GetCurrentMethod().Name);
+
+            IList<string> errorMessages;
+            bool valid = json.IsValid(schema, out errorMessages);
+
+            Assert.True(valid, errorMessages.FirstOrDefault());
+        }
+
+        [Fact]
+        public void ObjectEventPassQuantityList()
+        {
+            var json = GetJson(GetType().Name, MethodBase.GetCurrentMethod().Name);
+
+            IList<string> errorMessages;
+            bool valid = json.IsValid(schema, out errorMessages);
+
+            Assert.True(valid, errorMessages.FirstOrDefault());
+        }
+
+        [Fact]
+        public void ObjectEventPassBoth()
+        {
+            var json = GetJson(GetType().Name, MethodBase.GetCurrentMethod().Name);
+
+            IList<string> errorMessages;
+            bool valid = json.IsValid(schema, out errorMessages);
+
+            Assert.True(valid, errorMessages.FirstOrDefault());
+        }
+
+        [Fact]
+        public void ObjectEventFailNone()
+        {
+            var json = GetJson(GetType().Name, MethodBase.GetCurrentMethod().Name);
+
+            IList<string> errorMessages;
+            bool valid = json.IsValid(schema, out errorMessages);
+
+            Assert.False(valid, errorMessages.FirstOrDefault());
+        }
+    }
+}
